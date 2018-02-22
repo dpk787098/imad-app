64550f7d893d77f73cddab02423e6f82f31970b4
Deepak Kumar <deepak787098@gmail.com>
2018-02-22 20:36:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/counter' , function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name', function(req, res) { //URL: /submit-name?name=xxxx
+app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
     //get the name from request
     var name = req.query.name;
     
