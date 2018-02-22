218f113aa9cfd80db9183725388ea82b91b3e4dc
Deepak Kumar <deepak787098@gmail.com>
2018-02-22 16:13:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/:articleName', function (req, res) {
     res.send(createTemplate(articles[articleName]));
 });
  
- app.get('/' , function (req , res){
+app.get('/ui/index.html' , function (req , res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
 });
  
