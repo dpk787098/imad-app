99f7f8bc70011ad4e9d0edc376212b6a1e2ae46f
Deepak Kumar <deepak787098@gmail.com>
2018-02-22 19:17:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,15 @@
-<!doctype html>
-<html>
-    <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div class="center">
-                <img id='madi' src="https://scontent.fhyd4-1.fna.fbcdn.net/v/t1.0-9/14463140_964423330333293_2317178152061082515_n.jpg?oh=a6d9b726b32335bb256b57cd045ee50f&oe=5B24010A" class="img-medium"/>
-            </div>
-            <h3>Personal</h3>
-			<div>
-				Hi, My name is Deepak Kumar
-			</div>
-			<hr/>
-			<h3>Professional</h3>
-			<div>
-				I am a Student
-			</div>
-			<hr/>
-			<div  class="footer">
-			    This button <button id="counter">Click me!</button> has been clicked<span id="count">0</span>times.
-			</div>
-		</div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
-</html>
+//Counter code
+var button = document.getElementById("counter");
+var counter = 0;
+
+button.onclick = function() {
+    
+    //make a request to the counter
+    
+    //capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+};
