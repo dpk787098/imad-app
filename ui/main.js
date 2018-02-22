5aca04b03540b4ece0bb7e14fbf87ea03d066ac3
Deepak Kumar <deepak787098@gmail.com>
2018-02-22 19:10:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,28 @@
-//counter code
-var button = document.getElementById('counter');
-
-button.onclick = function () {
-  
-  //create a request object
-  var request = new XMLHttpRequest();
-      
-  //capture the response and store it in a variable
-  request.onreadystatechange = function () {
-      if (request.readystate === XMLHttpRequest.DONE) {
-          //take some action      
-          if(request.status === 200) {
-              var counter = request.responseText;
-              var span = document.getElementById("count");
-              span.innerHTML = counter.toString();
-            }
-        }
-        //not done yet
-    };
-    
-    //make a request
-    request.open('GET', 'http://deepak787098.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
+<!doctype html>
+<html>
+    <head>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="center">
+                <img id='madi' src="https://scontent.fhyd4-1.fna.fbcdn.net/v/t1.0-9/14463140_964423330333293_2317178152061082515_n.jpg?oh=a6d9b726b32335bb256b57cd045ee50f&oe=5B24010A" class="img-medium"/>
+            </div>
+            <h3>Personal</h3>
+			<div>
+				Hi, My name is Deepak Kumar
+			</div>
+			<hr/>
+			<h3>Professional</h3>
+			<div>
+				I am a Student
+			</div>
+			<hr/>
+			<div  class="footer">
+			    This button <button id="counter">Click me!</button> has been clicked<span id="count">0</span>times.
+			</div>
+		</div>
+        <script type="text/javascript" src="/ui/main.js">
+        </script>
+    </body>
+</html>
