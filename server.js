f42de8c0e48addccd9f22f6893bcab575e14019e
Deepak Kumar <deepak787098@gmail.com>
2018-02-22 20:28:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ app.get('/ui/main.js', function (req, res) {
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
 
 var names = [];
 app.get('/submit-name/:name', function(req, res) {
