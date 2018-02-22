f59bcf57489c3cef98e5ba666c237d4b209b20e4
Deepak Kumar <deepak787098@gmail.com>
2018-02-22 19:25:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,12 +4,23 @@ var counter = 0;
 
 button.onclick = function() {
     
-    //make a request to the counter
+    //create a request object
+    var request = new XMLHttpRequest();
     
     //capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if (request.sttus === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById("count");
+                span.innerHTML = counter.toString();
+            }
+        }
+        //not done yet
+    }
     
-    //Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById("count");
-    span.innerHTML = counter.toString();
+    //make a request
+    request.open('GET', 'http://deepak787098.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
