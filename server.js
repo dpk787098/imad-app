158a070dbd559a0a41e90a38c462dd6c272b4b46
Deepak Kumar <deepak787098@gmail.com>
2018-02-26 17:27:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,13 @@ app.get('/counter', function (req, res) {
    res.send(counter.toString());
 });
 
+app.get('/:articleName', function (req, res) {
+    //article == article-one
+    //articles[articleName] == {} content object for article one
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
+});
+
 var names = [];
 app.get('/submit-name', function(req, res) {
     //get the name from request
@@ -97,15 +104,7 @@ app.get('/submit-name', function(req, res) {
     names.push(name);
     //JSON: Javascript Object Notation
     res.send(JSON.stringify(names));
-});
-
-app.get('/:articleName', function (req, res) {
-    //article == article-one
-    //articles[articleName] == {} content object for article one
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
-});
- 
+}); 
  
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
