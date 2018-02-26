c7654c5891ec13240b3ded72adedc3cc62dcf852
Deepak Kumar <deepak787098@gmail.com>
2018-02-26 13:38:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,12 @@ app.get('/' , function (req , res) {
    res.sendFile(path.join(__dirname, 'ui', 'index.html')); 
 });
 
+var counter = 0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
 var names = [];
 app.get('/submit-name', function(req, res) {
     //get the name from request
@@ -93,13 +99,7 @@ app.get('/submit-name', function(req, res) {
     res.send(JSON.stringify(names));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-   counter = counter + 1;
-   res.send(counter.toString());
-});
-
-app.get('/:articleName', function (req, res) {
+app.get('/articleName', function (req, res) {
     //article == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
