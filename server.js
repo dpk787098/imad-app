3eb5664a2f3bd2c6b22ec9e7429afc0b45a91dca
Deepak Kumar <deepak787098@gmail.com>
2018-02-27 12:05:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,8 +78,8 @@ function createTemplate(data){
 return htmlTemplate;
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 var counter = 0;
@@ -89,38 +89,32 @@ app.get("/counter",function(req,res){
 });
 
 var names=[];
-app.get('/submit-name',function(req,res){ //submit-name?name=hitesh
-    var name=req.query.name;
+app.get('/submit-name', function(req, res) {
+    //get the name from request
+    var name = req.query.name;
+    
     names.push(name);
-    //JSON
+    //JSON: Javascript Object Notation
     res.send(JSON.stringify(names));
-});
+}); 
 
-
-app.get('/:articleName',function(req,res){
-    //articleName = article-one
+app.get('/:articleName', function(req, res) {
+    //article == article-one
+    //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
-     res.send(createTemplate(articles[articleName]));
+    res.send(createTemplate(articles[articleName]));
+});
+ 
+app.get('/ui/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-two',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+app.get('/ui/main.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/article-three',function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/madi.png', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
@@ -129,6 +123,6 @@ app.get('/ui/madi.png', function (req, res) {
 
 var port = 80;
 app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+    console.log(`IMAD course app listening on port ${port}!`);
 });
  
