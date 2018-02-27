f7f8ec9f69011956fcf688cedd3f9d7ac275ff78
Deepak Kumar <deepak787098@gmail.com>
2018-02-27 12:20:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,61 @@
-<!doctype html>
-<html>
-    <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div class="center">
-                <img id='madi' src="https://scontent.fhyd4-1.fna.fbcdn.net/v/t1.0-9/14463140_964423330333293_2317178152061082515_n.jpg?oh=a6d9b726b32335bb256b57cd045ee50f&oe=5B24010A" class="img-medium"/>
-            </div>
-            <h3>Personal</h3>
-			<div>
-				Hi, My name is Deepak Kumar
-			</div>
-			<hr/>
-			<h3>Professional</h3>
-			<div>
-				I am a Student
-			</div>
-			<hr/>
-			<div  class="footer">
-			    This button <button id="counter">Click me!</button> has been clicked<span id="count">0</span>times.
-			    <hr />
-			    <input type="text" id="name" placeholder="name"></input>
-			    <input type="submit" value="submit" id="submit_btn"></input>
-			    <ul id='namelist'>
-			    </ul>
-			</div>
-		</div>
-        <script type="text/javascript" src="/ui/main.js">
-        </script>
-    </body>
-</html>
+//counter code
+var button = document.getElementById('counter');
+
+button.onClick = function(){
+   
+    //create a request
+    var request = new XMLHttpRequest();
+   
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+   
+    //make a request
+    request.open('GET','http://deepak787098.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
+
+//submit name
+
+
+var submit = document.getElementById('submit_btn');
+submit.onClick = function(){
+  //make a request to the server and send the name
+   //create a request
+    var request = new XMLHttpRequest();
+   
+    //capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if(request.status === 200){
+              //create a list of name and render it as a list
+              var name = request.responseText;
+              names=JSON.parse(names);
+              var list = '';
+              for(var i=0 ; i<names.length;i++){
+                  list += '<li>'+names[i]+'<li>';
+              }
+              var ul = document.getElementById('nameList');
+              ul.innerHTML = list;  
+            }
+        }
+    };
+   
+    //make a request
+    var nameInput=document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://deepak787098.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(null);
+  
+  
+ 
+};
